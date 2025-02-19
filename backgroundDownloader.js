8c26ede1ecbb93938c1d6d99d43782d2009817da
Vlad Brinzas <vlad.brinzas@gmail.com>
2025-02-19 16:13:43 +0200

    Created alternate activation method by just clicking the extension icon
    Wrote the code that could be used to do the document > markdown > download
    chain directly upon the user clicking the extension icon (as opposed to
    opening a popup with a button).
    Left it commented-out, though, because the user experience feels less
    smooth this way (less clear feedback, easier to accidentally do a thing).

--- backgroundDownloader.js
+++ backgroundDownloader.js
@@ -1,3 +1,17 @@
+// Click-on-extension-item-to-use version
+// To enable:
+// - uncomment this code
+// - remove "default_popup": "popup.html" from "action" in manifest.json
+// - add "default_title": "Click to save description as markdown" to "action" in manifest.json
+//
+// Listen for extension icon being clicked
+// chrome.action.onClicked.addListener((tab) => {
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         files: [ "pageScript.js" ]
+//     });
+// })
+
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
     if (message.action === "download") {
 
