f0d79bc472642a9821969c41a1461a8c988ef3c6
Vlad Brinzas <vlad.brinzas@gmail.com>
2025-02-19 16:00:04 +0200

    Added logic to check user actually is on a LeetCode.com problem page.

--- popup.js
+++ popup.js
@@ -9,11 +9,16 @@
 
 // V2, callback-based
 function sendScanMessage() {
-    chrome.tabs.query({ active: true, currentWindow: true },
-        ([tab]) => chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: [ "pageScript.js" ]
-        })
+    chrome.tabs.query({ active: true, currentWindow: true, url: "https://leetcode.com/problems/*" },
+        ([tab]) => {
+            if (tab) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    files: [ "pageScript.js" ]
+                });
+            }
+            else alert("LeetCode to Markdown only works on LeetCode.com problem pages");
+        }
     );
 }
 
